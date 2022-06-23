3df1c54021cea3a3d35ba42ec26abc56a81e3761
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-06-23 15:10:26 +0530

    Update Rotation.js

--- components/Rotation.js
+++ components/Rotation.js
@@ -47,38 +47,7 @@ AFRAME.registerComponent("plane-rotation-reader", {
       var planePosition = this.data.speedOfAscent;
 
       //control the attributes with the Arrow Keys
-      if (e.key === "ArrowRight") {
-        if (planeRotation.x < 10) {
-          planeRotation.x += 0.5;
-          this.el.setAttribute("rotation", planeRotation);
-        }
-      }
-      if (e.key === "ArrowLeft") {
-        if (planeRotation.x > -10) {
-          planeRotation.x -= 0.5;
-          this.el.setAttribute("rotation", planeRotation);
-        }
-      }
-      if (e.key === "ArrowUp") {
-        if (planeRotation.z < 20) {
-          planeRotation.z += 0.5;
-          this.el.setAttribute("rotation", planeRotation);
-        }
-        if (planePosition.y < 2) {
-          planePosition.y += 0.01;
-          this.el.setAttribute("position", planePosition);
-        }
-      }
-      if (e.key === "ArrowDown") {
-        if (planeRotation.z > -10) {
-          planeRotation.z -= 0.5;
-          this.el.setAttribute("rotation", planeRotation);
-        }
-        if (planePosition.y > -2) {
-          planePosition.y -= 0.01;
-          this.el.setAttribute("position", planePosition);
-        }
-      }
+      
     });
   }
 });
